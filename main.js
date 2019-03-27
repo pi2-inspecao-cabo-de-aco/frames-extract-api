26340a65fe2de8ade7a09e01d3da4477d159f692
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-03-27 00:38:36 -0300

    Creating method to extract frames and others ajusts

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import extractFrames from 'ffmpeg-extract-frames'
 
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  console.log(`Running on ${port}...`)
-})
+try {
+  let extract = async e => {
+    await extractFrames({
+      input: './video.mp4',
+      output: './frames/frame-%d.png'
+    })
+  }
+  extract()
+} catch (err) {
+  console.log(err)
+}
 
+app.listen(port, () => {
+  console.log(`Running on port ${port}...`)
+})
